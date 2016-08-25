c0337d3958f1cbcd279821ac943cb80a183f0c18
Michal Špaček <tupinek@gmail.com>
2016-08-25 23:24:11 +0200

    Prepare for 0.07 version.

--- Check.pm
+++ Check.pm
@@ -9,7 +9,7 @@ use Error::Pure qw(err);
 use Error::Pure::Utils;
 
 # Version.
-our $VERSION = 0.06;
+our $VERSION = 0.07;
 
 # Error level.
 $Error::Pure::Utils::LEVEL = 5;
@@ -224,6 +224,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.06
+0.07
 
 =cut
