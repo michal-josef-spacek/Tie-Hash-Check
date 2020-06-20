060d54d34e02823b4608d5dff61ec6b9f8ea6534
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-06-20 18:07:39 +0200

    Improve LICENSE AND COPYRIGHT section in doc

--- Check.pm
+++ Check.pm
@@ -219,8 +219,9 @@ L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2009-2016 Michal Josef Špaček
- BSD 2-Clause License
+© 2009-2016 Michal Josef Špaček
+
+BSD 2-Clause License
 
 =head1 VERSION
 
